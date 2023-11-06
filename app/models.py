ef94ae09c2d92d703f678553467e9175f6ecd51d
kashishshrivastav <kashishshrvstv58@gmail.com>
2023-11-06 22:34:25 +0530

    uploaddata

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tesla(models.Model):
+    date = models.DateTimeField()
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    adj_close = models.FloatField()
+    volume = models.IntegerField()
